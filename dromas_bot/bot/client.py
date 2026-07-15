effb5868445298890d6fa31c343772feca6e7132
ghdfn2025-coder <ghdfn2025@gmail.com>
2026-07-15 14:49:20 +0900

    Update client.py

--- dromas_bot/bot/client.py
+++ dromas_bot/bot/client.py
@@ -41,7 +41,7 @@ class DromasBot(commands.Bot):
 
     async def on_ready(self) -> None:
         activity = discord.CustomActivity(
-            name="/도움말 | 개발: 모카"
+            name="/도움말 | 선로 서버장 개발"
         )
 
         await self.change_presence(
